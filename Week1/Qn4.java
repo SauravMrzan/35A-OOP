2c4017d1fb1f98d7a191c70c07bd70fdef241335
Saurav <maharjansaurav2002@gmail.com>
2024-04-11 10:55:23 +0545

    Week 1 Tasks

---
+++ Week1/Qn4.java
@@ -0,0 +1,17 @@
+package Tasks.Week1;
+
+public class Qn4 {
+    public static void main(String[] args) {
+        /*4. WAP that prints the following pattern:
+            *
+            **
+            ***
+            ****    */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        
+
+    }
+}
---
+++ Week1/Qn5.java
@@ -0,0 +1,15 @@
+package Tasks.Week1;
+
+public class Qn5 {
+    public static void main(String[] args) {
+        /*5. WAP prints the following pattern:
+            *********
+            *********
+            *********
+            *********    */
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+    }
+}
