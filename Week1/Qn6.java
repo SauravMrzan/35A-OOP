ce019727bc9a1116c1c46175e4147f57d00a72a2
Saurav <maharjansaurav2002@gmail.com>
2024-04-11 22:06:05 +0545

    Completed

--- Week1/Qn6.java
+++ Week1/Qn6.java
@@ -15,7 +15,6 @@ public class Qn6 {
 
             One is what we see when we're typing our program. The other is what appears on the "console."
             */
-        System.out.print("A well-formed Java program has a main method with { and } braces.\n \nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \"\\ instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"\"?
-        ");
+        System.out.print("A well-formed Java program has a main method with { and } braces.\n \n \nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \\\" instead!)\nWhat is the difference between\na ' and a \"? Or between a \" and a \\\"?\n\nOne is what we see when we're typing our program. The other is what appears on the \"console.\" ");
     }
 }
